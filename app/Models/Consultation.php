c573d09fe9bca31ab3c805808c13fe27405a2d3b
mostafaza <mohatrt99@gmail.com>
2021-06-16 11:28:43 +0200

    new features for consultation

---
+++ app/Models/Consultation.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Consultation extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'motif',
+        'confirmation',
+        'user_id',
+    ];
+
+   
+
+   	 public function user()
+    {
+    	return $this->belongsTo(User::class,'user_id','id');
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -42,4 +42,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+  
+
+
+    public function consultation()
+    {
+        return $this->hasMany(Consultation::class,'id');
+    }
 }
